38d213f89a2ff4b9e76454e8e497e04827087891
niferreira <nicolas.ferreira64680@live.fr>
2020-06-01 09:43:11 +0200

    Ajout des sources

---
+++ exo-test.test/ClassTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using exo_tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace exo_test.test
+{
+    [TestClass]
+    public class ClassTest
+    {
+        [TestMethod]
+        public void ValidParameters()
+        {
+            string initiale = ClasseChaine.RetourneInitiales("Nicolas", "FERREIRA");
+            Assert.AreEqual(initiale, "NF");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BadParameters()
+        {
+            ClasseChaine.RetourneInitiales(null, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BadParameters2()
+        {
+            ClasseChaine.RetourneInitiales("Nicolas", null);
+        }
+
+        [TestMethod]
+        public void MultipleName()
+        {
+            string initiale = ClasseChaine.RetourneInitiales("Nicolas Jean", "FERREIRA AA");
+            Assert.AreEqual(initiale, "NJFA");
+        }
+    }
+}
---
+++ exo-tests/ClasseChaine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo_tests
+{
+    public static class ClasseChaine
+    {
+        public static string RetourneInitiales(string prenom, string nom)
+        {
+            string initiale = "";
+            if (string.IsNullOrEmpty(prenom) || string.IsNullOrEmpty(nom))
+            {
+                throw new ArgumentNullException();
+            }
+
+            string[] listPrenom = prenom.Split(' ');
+            string[] listNom = nom.Split(' ');
+
+            foreach (var item in listPrenom)
+            {
+                initiale += item.Substring(0, 1);
+            }
+
+            foreach (var item in listNom)
+            {
+                initiale += item.Substring(0, 1);
+            }
+
+            return initiale;
+        }
+    }
+}
